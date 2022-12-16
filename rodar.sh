b6872969f9e72ceb7fa3cbcbf60d375a9a435bf6
Emanuel Galhardo <emanuel.df@gmail.com>
2022-12-16 11:59:04 -0300

    Running adjustments

--- rodar.sh
+++
@@ -1,6 +0,0 @@
-docker-compose up -d influxdb grafana
-docker-compose run k6 run scripts/1-smoke.js
-# docker-compose run k6 run scripts/2-load.js
-# docker-compose run k6 run scripts/3-stress.js
-# docker-compose run k6 run scripts/4-spike.js
-# docker-compose run k6 run scripts/5-soak.js
